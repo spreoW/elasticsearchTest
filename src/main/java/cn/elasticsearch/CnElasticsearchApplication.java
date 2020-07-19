96c1d21ec75679af18dc80f0d50354a0e8bb99de
wq <1563904374@qq.com>
2020-07-19 10:12:44 +0800

    first commit

---
+++ src/main/java/cn/elasticsearch/CnElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package cn.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/elasticsearch/CnElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
