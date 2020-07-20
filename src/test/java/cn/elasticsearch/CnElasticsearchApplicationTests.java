a2b998f945f1c6e7765bcf374a9b1bacbb4a5e9c
wq <1563904374@qq.com>
2020-07-20 08:06:09 +0800

    有错误

--- src/test/java/cn/elasticsearch/CnElasticsearchApplicationTests.java
+++ src/test/java/cn/elasticsearch/CnElasticsearchApplicationTests.java
@@ -1,13 +1,27 @@
 package cn.elasticsearch;
 
+import cn.elasticsearch.config.ESConfig;
+import cn.elasticsearch.pojo.Item;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
 @SpringBootTest
 class CnElasticsearchApplicationTests {
+	@Autowired
+	private ElasticsearchRestTemplate elasticsearchRestTemplate;
 
 	@Test
 	void contextLoads() {
+		ESConfig esConfig = new ESConfig();
+		//RestHighLevelClient restHighLevelClient = esConfig.elasticsearchClient();
+		elasticsearchRestTemplate.createIndex(Item.class);
+		elasticsearchRestTemplate.putMapping(Item.class);
+
 	}
 
 }
