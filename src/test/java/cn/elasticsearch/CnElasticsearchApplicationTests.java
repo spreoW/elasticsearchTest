b0529e9b12b4f4cebfed42a6a8b1176d16aaf639
wq <1563904374@qq.com>
2020-07-26 21:31:54 +0800

    爬虫

--- src/test/java/cn/elasticsearch/CnElasticsearchApplicationTests.java
+++ src/test/java/cn/elasticsearch/CnElasticsearchApplicationTests.java
@@ -3,6 +3,8 @@ package cn.elasticsearch;
 import cn.elasticsearch.pojo.User;
 import com.alibaba.fastjson.JSON;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
@@ -13,6 +15,7 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,17 +65,30 @@ public class CnElasticsearchApplicationTests {
 		System.out.println(acknowledgedResponse.isAcknowledged());
 	}
 
+	/**
+	 * 向index添加一个User
+	 */
 	@Test
 	public void addDocument() throws IOException {
-		User user = new User("wq",22);
+		User user = new User("zs",33);
 		IndexRequest indexRequest = new IndexRequest("wq6_index");
 		indexRequest.timeout(TimeValue.timeValueSeconds(1L));
 		indexRequest.timeout("1s");
 		indexRequest.type("String");
 		//将数据放入请求
 		indexRequest.source(JSON.toJSON(user), XContentType.JSON);
-
 		IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-		System.out.println(indexRequest.toString());
+		System.out.println(indexResponse.toString());
+		System.out.println(indexResponse.status());
 	}
+
+	/**
+	 * 向index添加一个User
+	 */
+//	@Test
+//	public void getDocument() throws IOException {
+//		GetRequest getRequest = new GetRequest("wq6_index", "1");
+//		getRequest.fetchSourceContext(new FetchSourceContext(false));
+//		getRequest.storedFields("_none_");
+//	}
 }
